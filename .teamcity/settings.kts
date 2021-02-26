3645058b210f2a6598d2b86aec7ae908827bff0f
Administrator <Administrator@TeamCity>
2021-02-26 12:50:20 -0600

    TeamCity change in 'Coffee Machine' project: Synchronization with own VCS root is enabled

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,11 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.azureDevopsConnection
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.dockerRegistry
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -23,25 +29,205 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2020.1"
+version = "2020.2"
 
 project {
 
+    vcsRoot(AzureCoffeeMachine)
+
+    buildType(ManualDeploy)
+    buildType(PullRequestTest)
+    buildType(SetVersion)
+    buildType(BuildStreamjockey)
     buildType(Build)
+
+    features {
+        azureDevopsConnection {
+            id = "PROJECT_EXT_13"
+            displayName = "Azure DevOps"
+            serverUrl = "https://dev.azure.com/eborchardt/"
+            accessToken = "credentialsJSON:9399bc85-a065-4f7a-9926-e5a206f08856"
+        }
+        dockerRegistry {
+            id = "PROJECT_EXT_20"
+            name = "Docker Registry"
+            url = "https://docker.io"
+            userName = "xxandorra"
+            password = "credentialsJSON:fdbfb5c9-8963-4e21-9241-93e77e0ad70a"
+        }
+    }
 }
 
 object Build : BuildType({
     name = "Build"
 
+    artifactRules = """
+        .about-version
+        README.md
+    """.trimIndent()
+
+    params {
+        param("teamcity.vcsTrigger.mergedBranchesThreshold", "3")
+        checkbox("Confirmation", "false", label = "Confirmation", display = ParameterDisplay.PROMPT,
+                  checked = "true", unchecked = "false")
+        text("ConfirmationCheck", "%Confirmation%", display = ParameterDisplay.HIDDEN,
+              regex = "^true${'$'}", validationMessage = "Confirmation Required")
+        param("teamcity.vcsTrigger.analyzeFullHistoryForMergeCommits", "false")
+        param("teamcity.vcsTrigger.analyzeAllPendingChangesOnMergeCommit", "false")
+    }
+
     vcs {
         root(DslContext.settingsRoot)
     }
 
+    steps {
+        script {
+            scriptContent = """echo "Hi""""
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+        script {
+            scriptContent = "echo ##teamcity[setParameter name='secretpassword' value='youcantseethis' type='password']"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
+
     triggers {
         vcs {
-            triggerRules = "+:root=${DslContext.getParameter("vcsroot")}:.about-version"
-
+            triggerRules = "+:/Home/"
             branchFilter = ""
+            enableQueueOptimization = false
         }
+    }
+
+    features {
+        dockerSupport {
+            cleanupPushedImages = true
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_20"
+            }
+        }
+    }
+
+    requirements {
+        doesNotEqual("env.OS", "Windows_NT")
+    }
+})
+
+object BuildStreamjockey : BuildType({
+    name = "build streamjockey"
+
+    params {
+        param("PerforceStream", "")
+    }
+
+    steps {
+        script {
+            scriptContent = "echo %PerforceStream%"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
+
+    dependencies {
+        snapshot(SetVersion) {
+            synchronizeRevisions = false
+        }
+    }
+})
+
+object ManualDeploy : BuildType({
+    name = "manual deploy"
+
+    params {
+        param("JavaVersion", "java-1.8.0-amazon-corretto-devel-1-SNAPSHOT")
+        text("reverse.dep.*.PerforceStream", "java-1.8.0-amazon-corretto-devel-1-SNAPSHOT", label = "Set Build Version PerforceStream", description = "Enter a Stream Name", display = ParameterDisplay.PROMPT, allowEmpty = false)
+    }
+
+    steps {
+        script {
+            name = "Convert Characters to Lowercase"
+            scriptContent = """
+                javaversion=${'$'}(echo %JavaVersion% | tr '[:upper:]' '[:lower:]')
+                echo "##teamcity[setParameter name='JavaVersion' value='${'$'}javaversion']"
+            """.trimIndent()
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+        script {
+            scriptContent = "echo %JavaVersion%"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
+
+    dependencies {
+        snapshot(BuildStreamjockey) {
+            synchronizeRevisions = false
+        }
+    }
+
+    requirements {
+        doesNotEqual("env.OS", "Windows_NT")
+    }
+})
+
+object PullRequestTest : BuildType({
+    name = "PullRequestTest"
+    paused = true
+
+    vcs {
+        root(AzureCoffeeMachine)
+    }
+
+    triggers {
+        vcs {
+            triggerRules = "+:/Home/"
+            branchFilter = "+:pull/*"
+        }
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${AzureCoffeeMachine.id}"
+            provider = azureDevOps {
+                authType = token {
+                    token = "credentialsJSON:9494d50d-8c05-41d0-8ada-db7aebe02da1"
+                }
+            }
+        }
+    }
+})
+
+object SetVersion : BuildType({
+    name = "set version"
+
+    params {
+        param("PerforceStream", "")
+    }
+
+    steps {
+        script {
+            scriptContent = "echo %PerforceStream%"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
+})
+
+object AzureCoffeeMachine : GitVcsRoot({
+    name = "AzureCoffeeMachine"
+    url = "https://eborchardt@dev.azure.com/eborchardt/Coffee-Machine/_git/Coffee-Machine"
+    branch = "refs/heads/master"
+    authMethod = password {
+        userName = "administrator"
+        password = "credentialsJSON:9399bc85-a065-4f7a-9926-e5a206f08856"
     }
 })

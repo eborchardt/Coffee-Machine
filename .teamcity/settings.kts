926f41fdbcd97a7237a083a588d2ca77c8c8dc04
Administrator <Administrator@TeamCity>
2021-03-01 11:06:08 -0600

    TeamCity change in 'Coffee Machine' project: New artifacts storage settings added: S3 Storage (type: S3_storage, id: NEW_STORAGE)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -56,6 +56,21 @@ project {
             userName = "xxandorra"
             password = "credentialsJSON:fdbfb5c9-8963-4e21-9241-93e77e0ad70a"
         }
+        feature {
+            id = "PROJECT_EXT_21"
+            type = "storage_settings"
+            param("aws.service.endpoint", "http://192.168.0.63:4566")
+            param("secure:aws.secret.access.key", "credentialsJSON:bfedcbdc-5db8-450f-b279-3d9182096e20")
+            param("aws.external.id", "TeamCity-server-9eec4a2a-59ee-45ac-93f3-917eac33b497")
+            param("aws.environment", "custom")
+            param("storage.name", "asdf")
+            param("storage.s3.bucket.name", "onexlab")
+            param("storage.type", "S3_storage")
+            param("aws.access.key.id", "test")
+            param("aws.credentials.type", "aws.access.keys")
+            param("aws.region.name", "us-east-1")
+            param("storage.s3.upload.presignedUrl.enabled", "true")
+        }
     }
 
     subProject(Subproject)

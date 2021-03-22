195d45973ac58c5421410e43c69b86bfa93546c4
Administrator <Administrator@TeamCity>
2021-03-22 13:37:29 -0500

    TeamCity change in 'Coffee Machine' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -171,7 +171,7 @@ object Build : BuildType({
         dockerCommand {
             id = "RUNNER_126"
             commandType = push {
-                namesAndTags = "http://192.168.0.63:5000/figlet:ubuntu"
+                namesAndTags = "192.168.0.63:5000/figlet:ubuntu"
             }
         }
     }

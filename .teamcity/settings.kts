266a2e30ec4916ecfd92f7023376021027894984
Administrator <Administrator@TeamCity>
2021-05-07 15:45:01 -0500

    TeamCity change in 'Coffee Machine' project: runners of 'NoSources' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -363,13 +363,7 @@ object NoSources : BuildType({
         }
         powerShell {
             scriptMode = script {
-                content = """
-                    ${'$'}warning = write-warning "This is a warning. You have been warned."
-                    "${'$'}([char]27)[${'$'}('1;35')m${'$'}warning${'$'}([char]27)[0m"
-                    
-                    
-                    #"${'$'}([char]27)[${'$'}('1;35')m${'$'}{Text}${'$'}([char]27)[0m"
-                """.trimIndent()
+                content = """write-warning "This is a warning. You have been warned." 1>2"""
             }
             param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
             param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")

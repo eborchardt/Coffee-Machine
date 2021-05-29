c14bee7fc355d46581f88e83f030f1c98f843da8
Administrator <Administrator@TeamCity>
2021-05-28 21:13:02 -0500

    TeamCity change in 'Coffee Machine' project: build features of 'build streamjockey' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -331,7 +331,7 @@ object BuildStreamjockey : BuildType({
         }
         investigationsAutoAssigner {
             id = "InvestigationsAutoAssigner"
-            defaultAssignee = "Administrator"
+            defaultAssignee = "administrator"
         }
     }
 

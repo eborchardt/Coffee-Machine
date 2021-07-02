d83060fd589af46664096d0f4f28ae9aa5d76b26
Administrator <Administrator@TeamCity>
2021-07-01 21:36:41 -0500

    TeamCity change in 'Coffee Machine / as' project: runners of 'af' build configuration were updated

--- .teamcity/patches/buildTypes/As_Af.kts
+++ .teamcity/patches/buildTypes/As_Af.kts
@@ -12,5 +12,59 @@ in the project with id = 'As', and delete the patch script.
 create(RelativeId("As"), BuildType({
     id("As_Af")
     name = "af"
+
+    steps {
+        step {
+            type = "MRPP_xunit_dotcover"
+            executionMode = BuildStep.ExecutionMode.DEFAULT
+            param("xUnitNet.dotCover.reportType", "HTML")
+            param("xUnitNet.executable.legacymode", "false")
+            param("xUnitNet.dotCover.reportOutputFile", "dotCoverReport.html")
+            param("xUnitNet.assembliesExcludePath", "")
+            param("xUnitNet.dotCover.AttributeFilters", """
+                <!-- Attribute filters. It's possible to use asterisks as wildcard symbols. -->
+                <AttributeFilters>
+                  <AttributeFilterEntry>System.CodeDom.Compiler.GeneratedCodeAttribute</AttributeFilterEntry>
+                  <AttributeFilterEntry>System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute</AttributeFilterEntry>
+                  <AttributeFilterEntry>System.ObsoleteAttribute</AttributeFilterEntry>
+                </AttributeFilters>
+            """.trimIndent())
+            param("xUnitNet.dotCover.enable", "true")
+            param("xUnitNet.nugetExe", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
+            param("xUnitNet.assembliesPath", """
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net452\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net46\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net461\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net462\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net47\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net471\xunit.console.exe 
+                D:\BuildAgent\work\509a440f2d235307\xunit.runner.console.2.4.1\tools\net472\xunit.console.exe
+            """.trimIndent())
+            param("xUnitNet.dotCover.skipProcesses", "")
+            param("xUnitNet.dotCover.exportReport", "false")
+            param("xUnitNet.notrait", "Category=Integration")
+            param("xUnitNet.nugetSource", "http://www.nuget.org/api/v2/")
+            param("xUnitNet.trait", "")
+            param("xUnitNet.executable", "xunit.console.exe")
+            param("xUnitNet.dotCover.Filters", """
+                <!-- Coverage filters. It's possible to use asterisks as wildcard symbols. -->
+                <Filters>
+                  <IncludeFilters>
+                    <FilterEntry>
+                      <ModuleMask>*</ModuleMask>
+                      <ClassMask>*</ClassMask>
+                      <FunctionMask>*</FunctionMask>
+                    </FilterEntry>
+                  </IncludeFilters>
+                  <ExcludeFilters>
+                    <FilterEntry>
+                      <ModuleMask>*Tests</ModuleMask>
+                    </FilterEntry>
+                  </ExcludeFilters>
+                </Filters>
+            """.trimIndent())
+            param("xUnitNet.executable.args", "")
+        }
+    }
 }))
 

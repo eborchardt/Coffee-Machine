25344f3f127744d783cf4f7794a519a70069c330
Administrator <Administrator@TeamCity>
2021-09-22 13:42:44 -0500

    TeamCity change in 'Coffee Machine' project: Cleanup old configuration files

--- .teamcity/Subproject2/Project.kt
+++
@@ -1,9 +0,0 @@
-package Subproject2
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.Project
-
-object Project : Project({
-    id("Subproject2")
-    name = "subproject2"
-})
